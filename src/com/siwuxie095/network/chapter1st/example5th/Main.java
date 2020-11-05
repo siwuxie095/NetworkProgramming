5d97e3d78de538dfbbbe188d969ca6ff1f3df6b6
siwuxie095 <834879583@qq.com>
2020-11-05 21:53:48 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter1st/example5th/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.network.chapter1st.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-05 21:50:11
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里介绍了 Netty 框架的背景知识，包括 Java 网络编程 API 的演变过程，阻塞和非阻塞网络操作之间的区别，以及
+     * 异步 I/O 在高容量、高性能的网络编程中的优势。
+     *
+     * 然后概述了 Netty 的特性、设计和优点，其中包括 Netty 异步模型的底层机制，包括回调、Future 以及它们的结合
+     * 使用。另外还谈到了事件是如何产生的以及如何拦截和处理它们。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
