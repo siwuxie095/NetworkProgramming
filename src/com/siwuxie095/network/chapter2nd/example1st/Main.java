e4c8413b0679b33197dae65dd8d3aa1ee495f33b
siwuxie095 <834879583@qq.com>
2020-11-06 08:09:46 +0800

    你的第一款 Netty 应用程序；

---
+++ src/com/siwuxie095/network/chapter2nd/example1st/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.network.chapter2nd.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-06 08:06:06
+ */
+public class Main {
+
+    /**
+     * 你的第一款 Netty 应用程序
+     *
+     * 这里将展示如何构建一个基于 Netty 的客户端和服务器。应用程序很简单：客户端将消息发送给服务器，
+     * 而服务器再将消息回送给客户端。
+     *
+     * 通过这个应用程序，你将获得关于 Netty 的一个关键方面的实践经验：通过 ChannelHandler 来构建
+     * 应用程序的逻辑。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
