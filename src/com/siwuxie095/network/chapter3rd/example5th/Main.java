9cc954e01834e92bd53c17bb2d4e738366a3e42f
siwuxie095 <834879583@qq.com>
2020-11-08 21:33:02 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter3rd/example5th/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.network.chapter3rd.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-08 21:30:00
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里从技术和体系结构这两个角度探讨了理解 Netty 的重要性。也更加详细地重新审视了之前引入的一些概念和组件，
+     * 特别是 ChannelHandler、ChannelPipeline 和引导。
+     *
+     * 特别地，这里讨论了 ChannelHandler 类的层次结构，并介绍了编码器和解码器，描述了它们在数据和网络字节格式
+     * 之间来回转换的互补功能。
+     *
+     * 下面将致力于深入研究这些组件，而这里所呈现的概览则有助于对整体的把控。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
