7b4a928cca5a23a90c7378a9b441991527c84738
siwuxie095 <834879583@qq.com>
2020-11-08 17:39:45 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter2nd/example6th/Main.java
@@ -0,0 +1,23 @@
+package com.siwuxie095.network.chapter2nd.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-08 17:37:01
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里构建和运行了你的第一款 Netty 客户端和服务器。虽然这只是一个简单的应用程序，但是它可以伸缩到支持
+     * 数千个并发连接 —— 每秒可以处理比普通的基于套接字的 Java 应用程序多得多的消息。
+     *
+     * 下面将看到更多关于 Netty 如何简化可伸缩性和并发性的例子。同时，也将更加深入地了解 Netty 对于关注点
+     * 分离的架构原则的支持。通过提供正确的抽象来解耦业务逻辑和网络编程逻辑，Netty 使得可以很容易地跟上快速
+     * 演化的需求，而又不危及系统的稳定性。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
