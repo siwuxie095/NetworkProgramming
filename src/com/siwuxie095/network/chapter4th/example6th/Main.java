0a4ea5f648bc553ddf99a7142eea328800404b67
siwuxie095 <834879583@qq.com>
2020-11-11 20:58:10 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter4th/example6th/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.network.chapter4th.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-11 20:54:00
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里研究了多种传输、它们的实现和使用，以及 Netty 是如何将它们呈现给开发者的。并深入探讨了 Netty 预置的传输，
+     * 并且解释了它们的行为。因为不是所有的传输都可以在相同的 Java 版本下工作，并且其中一些可能只在特定的操作系统下
+     * 可用，所以也描述了它们的最低需求。最后，讨论了如何匹配不同的传输和特定用例的需求。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
