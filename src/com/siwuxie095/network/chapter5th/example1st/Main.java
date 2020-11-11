f28c16f653d3ee20c016f9b9980e203df71a6bb0
siwuxie095 <834879583@qq.com>
2020-11-11 21:04:05 +0800

    ByteBuf；

---
+++ src/com/siwuxie095/network/chapter5th/example1st/Main.java
@@ -0,0 +1,25 @@
+package com.siwuxie095.network.chapter5th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-11 20:59:14
+ */
+public class Main {
+
+    /**
+     * ByteBuf
+     *
+     * 网络数据的基本单位总是字节。Java NIO 提供了 ByteBuffer 作为它的字节容器，但是这个类使用起来过于复杂，
+     * 而且也有些繁琐。
+     *
+     * Netty 的 ByteBuffer 替代品是 ByteBuf，一个强大的实现，既解决了 JDK API 的局限性，又为网络应用程序
+     * 的开发者提供了更好的 API。
+     *
+     * 这里将会说明和 JDK 的 ByteBuffer 相比，ByteBuf 的卓越功能性和灵活性。这也将有助于更好地理解 Netty
+     * 数据处理的一般方式。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
