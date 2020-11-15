533dbddd01c979a74def2f51b40bac29bab11675
siwuxie095 <834879583@qq.com>
2020-11-15 10:44:13 +0800

    ChannelHandler 和 ChannelPipeline；

---
+++ src/com/siwuxie095/network/chapter6th/example1st/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter6th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-15 10:36:07
+ */
+public class Main {
+
+    /**
+     * ChannelHandler 和 ChannelPipeline
+     *
+     * 在 ChannelPipeline 中可以将 ChannelHandler 链接在一起以组织处理逻辑。这里将会研究涉及这些类的各种用例，
+     * 以及一个重要的关系 — ChannelHandlerContext。
+     *
+     * 理解所有这些组件之间的交互对于通过 Netty 构建模块化的、可重用的实现至关重要。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
