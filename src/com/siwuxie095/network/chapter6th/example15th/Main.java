3ef61dd1944f64080f927a3b3af8880e4d9cfeae
siwuxie095 <834879583@qq.com>
2020-11-17 22:10:15 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter6th/example15th/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.network.chapter6th.example15th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-17 22:08:48
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里仔细地研究了 Netty 的数据处理组件 — ChannelHandler。讨论了 ChannelHandler 是如何
+     * 链接在一起，以及它们是如何作为 ChannelInboundHandler 和 ChannelOutboundHandler 与
+     * ChannelPipeline 进行交互的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
