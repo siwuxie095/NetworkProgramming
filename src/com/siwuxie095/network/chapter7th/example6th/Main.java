b2a8c52870a5d0c0389c7b25349f9f1d24fb1373
siwuxie095 <834879583@qq.com>
2020-11-20 22:01:05 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter7th/example6th/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.network.chapter7th.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-20 21:58:38
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 在这里，你了解了通常的线程模型，并且特别深入地学习了 Netty 所采用的线程模型，并详细探讨了其性能以及一致性。
+     *
+     * 你看到了如何在 EventLoop（I/O Thread）中执行自己的任务，就如同 Netty 框架自身一样。你学习了如何调度任务
+     * 以便推迟执行，并且还探讨了高负载下的伸缩性问题。你也看到了如何验证一个任务是否已被执行以及如何取消它。
+     *
+     * 通过对 Netty 框架的实现细节的研究所获得的这些信息，将帮助你在简化你的应用程序代码库的同时最大限度地提高它的
+     * 性能。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
