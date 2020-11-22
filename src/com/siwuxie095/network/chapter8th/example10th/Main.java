562c02e294cfeb33c0ba3be4644dc871f30148b8
siwuxie095 <834879583@qq.com>
2020-11-22 18:04:59 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter8th/example10th/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter8th.example10th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-22 18:03:20
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 在这里，你学习了如何引导 Netty 服务器和客户端应用程序，包括那些使用无连接协议的应用程序。同时也涵盖了
+     * 一些特殊情况，包括在服务器应用程序中引导客户端 Channel，以及使用 ChannelInitializer 来处理引导过程
+     * 中的多个 ChannelHandler 的安装。你看到了如何设置 Channel 的配置选项，以及如何使用属性来将信息附加到
+     * Channel。最后，你学习了如何优雅地关闭应用程序，以有序地释放所有的资源。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
