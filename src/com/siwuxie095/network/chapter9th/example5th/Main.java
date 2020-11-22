3f7b9fbb4d7ed68d51043032652cc157bd1e57a1
siwuxie095 <834879583@qq.com>
2020-11-22 22:32:21 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter9th/example5th/Main.java
@@ -0,0 +1,19 @@
+package com.siwuxie095.network.chapter9th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-22 22:31:07
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 使用 JUnit 这样的测试工具来进行单元测试是一种非常行之有效的方式，它能保证你的代码的正确性并提高它的可维护性。
+     * 在这里，你学习了如何使用 Netty 提供的测试工具来测试你自定义的 ChannelHandler。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
