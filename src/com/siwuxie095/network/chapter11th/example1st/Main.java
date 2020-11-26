6a0d53407e5abb5303ed0e7133d53b934dc19ef7
siwuxie095 <834879583@qq.com>
2020-11-27 07:59:01 +0800

    预置的 ChannelHandler 和编解码器；

---
+++ src/com/siwuxie095/network/chapter11th/example1st/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.network.chapter11th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-27 07:55:57
+ */
+public class Main {
+
+    /**
+     * 预置的 ChannelHandler 和编解码器
+     *
+     * Netty 为许多通用协议提供了编解码器和处理器，几乎可以开箱即用，这减少了你在那些相当繁琐的事务上
+     * 本来会花费的时间与精力。这里将会探讨这些工具以及它们所带来的好处，其中包括 Netty 对于 SSL/TLS
+     * 和 WebSocket 的支持，以及如何简单地通过数据压缩来压榨 HTTP，以获取更好的性能。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
