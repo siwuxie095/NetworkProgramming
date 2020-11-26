2cb6dd5b70a7b9267395b093d8e2e719a0354a5c
siwuxie095 <834879583@qq.com>
2020-11-26 08:37:03 +0800

    [update] Adjust;

---
+++ src/com/siwuxie095/network/chapter10th/example6th/Main.java
@@ -0,0 +1,22 @@
+package com.siwuxie095.network.chapter10th.example6th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-26 08:34:56
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 在这里，你学习了如何使用 Netty 的编解码器 API 来编写解码器和编码器。你也了解了为什么使用这个 API 相对于
+     * 直接使用 ChannelHandler API 更好。
+     *
+     * 你看到了抽象的编解码器类是如何为在一个实现中处理解码和编码提供支持的。如果你需要更大的灵活性，或者希望重用
+     * 现有的实现，那么你还可以选择结合它们，而无需扩展任何抽象的编解码器类。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
