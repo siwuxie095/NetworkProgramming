d7faaa301d57610387d22881d33adabca2d3b390
siwuxie095 <834879583@qq.com>
2020-11-29 17:31:21 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter11th/example8th/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter11th.example8th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-29 17:29:12
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * Netty 提供的编解码器以及各种 ChannelHandler 可以被组合和扩展，以实现非常广泛的处理方案。此外，
+     * 它们也是被论证的、健壮的组件，已经被许多的大型系统所使用。
+     *
+     * 需要注意的是，这里只涵盖了最常见的示例。如果想了解更多，Netty 的 API 文档提供了更加全面的覆盖。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
