91e0500e9594bba3016b4f81b0a5f718f93184b8
siwuxie095 <834879583@qq.com>
2020-11-30 07:56:04 +0800

    WebSocket 简介；

---
+++ src/com/siwuxie095/network/chapter12th/example2nd/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.network.chapter12th.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-11-30 07:51:02
+ */
+public class Main {
+
+    /**
+     * WebSocket 简介
+     *
+     * WebSocket 协议是完全重新设计的协议，旨在为 Web 上的双向数据传输问题提供一个切实可行的解决方案，使得客户端和服务器
+     * 之间可以在任意时刻传输消息，因此，这也就要求它们异步地处理消息回执。（作为 HTML5 客户端 API 的一部分，大部分最新的
+     * 浏览器都已经支持了 WebSocket。）
+     *
+     * Netty 对于 WebSocket 的支持包含了所有正在使用中的主要实现，因此在你的下一个应用程序中采用它将是简单直接的。和往常
+     * 使用 Netty 一样，你可以完全使用该协议，而无需关心它内部的实现细节。这里将通过创建一个基于 WebSocket 的实时聊天应
+     * 用程序来演示这一点。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
