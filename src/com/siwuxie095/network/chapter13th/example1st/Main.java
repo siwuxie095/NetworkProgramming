bcf6ccb0865d70d1575f62f1aefbbeee9b0e8dc5
siwuxie095 <834879583@qq.com>
2020-12-01 21:24:08 +0800

    使用 UDP 广播事件；

---
+++ src/com/siwuxie095/network/chapter13th/example1st/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.network.chapter13th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-01 08:32:57
+ */
+public class Main {
+
+    /**
+     * 使用 UDP 广播事件
+     *
+     * 到目前为止，你所见过的绝大多数的例子都使用了基于连接的协议，如 TCP。在这里，将会把重点放在一个无连接协议，
+     * 即 用户数据报协议（UDP）上，它通常用在性能至关重要并且能够容忍一定的数据包丢失的情况下。
+     *
+     * PS：最有名的基于 UDP 的协议之一便是域名服务（DNS），其将完全限定的名称映射为数字的 IP 地址。
+     *
+     * 这里将会首先概述 UDP 的特性以及它的局限性。在这之后，将描述示例应用程序，它会演示如何使用 UDP 的广播能力。
+     * 同时还会使用一个编码器和一个解码器来处理作为广播消息格式的 POJO。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
