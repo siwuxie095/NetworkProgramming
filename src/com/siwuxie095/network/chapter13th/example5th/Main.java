91cdb2466a5bdfa8ae551e81f4a7d912308702ba
siwuxie095 <834879583@qq.com>
2020-12-02 08:20:19 +0800

    消息 POJO：LogEvent；

---
+++ src/com/siwuxie095/network/chapter13th/example5th/Main.java
@@ -0,0 +1,24 @@
+package com.siwuxie095.network.chapter13th.example5th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-02 08:13:32
+ */
+public class Main {
+
+    /**
+     * 消息 POJO：LogEvent
+     *
+     * 在消息处理应用程序中，数据通常由 POJO 表示，除了实际上的消息内容，其还可以包含配置或处理信息。
+     * 在这个应用程序中，将会把消息作为事件处理，并且由于该数据来自于日志文件，所以将它称为 LogEvent。
+     *
+     * 以 LogEvent 为例，展示了这个简单的 POJO 的详细信息。
+     *
+     * 定义好了消息组件，便可以实现该应用程序的广播逻辑了。后续将研究用于编码和传输 LogEvent 消息的
+     * Netty 框架类。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
