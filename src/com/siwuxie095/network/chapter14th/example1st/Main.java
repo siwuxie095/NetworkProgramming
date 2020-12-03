c3324c118add0386400ccac6d8a56d097e0b4838
siwuxie095 <834879583@qq.com>
2020-12-04 07:57:56 +0800

    案例研究，第一部分；

---
+++ src/com/siwuxie095/network/chapter14th/example1st/Main.java
@@ -0,0 +1,23 @@
+package com.siwuxie095.network.chapter14th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-04 07:55:58
+ */
+public class Main {
+
+    /**
+     * 案例研究，第一部分
+     *
+     * 在这里，将介绍两部分案例研究中的第一部分，它们是由已经在内部基础设施中广泛使用了 Netty 的公司贡献的。
+     *
+     * 希望这些其他人如何利用 Netty 框架来解决现实世界问题的例子，能够拓展你对于 Netty 能够做到什么事情的
+     * 理解。
+     *
+     * PS：每个案例分析的作者都直接参与了他们所讨论的项目。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
