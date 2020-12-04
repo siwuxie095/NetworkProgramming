e470aec3ad2c79f1adaba2e8255f3256a09f11de
siwuxie095 <834879583@qq.com>
2020-12-04 08:04:31 +0800

    Droplr：构建移动服务；

---
+++ src/com/siwuxie095/network/chapter14th/example2nd/Main.java
@@ -0,0 +1,26 @@
+package com.siwuxie095.network.chapter14th.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-04 07:59:00
+ */
+public class Main {
+
+    /**
+     * Droplr：构建移动服务
+     *
+     * Bruno de Carvalho，首席架构师
+     *
+     * 在 Droplr，他们在他们的基础设施的核心部分、从他们的 API 服务器到辅助服务的各个部分 都使用了 Netty。
+     *
+     * 这是一个关于他们是如何从一个单片的、运行缓慢的 LAMP 应用程序迁移到基于 Netty 实现的现代的、高性能的
+     * 以及水平扩展的分布式架构的案例研究。
+     *
+     * PS：LAMP 即 一个典型的应用程序技术栈的首字母缩写。由 Linux、ApacheWebServer、MySQL 以及 PHP 的
+     * 首字母组成。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
