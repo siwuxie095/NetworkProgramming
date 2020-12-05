2276af60db2c35593b3f6bd5e736a7200eb73700
siwuxie095 <834879583@qq.com>
2020-12-05 22:28:57 +0800

    Netty 在 Twitter 的使用：Finagle；

---
+++ src/com/siwuxie095/network/chapter15th/example10th/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter15th.example10th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-05 21:04:19
+ */
+public class Main {
+
+    /**
+     * Netty 在 Twitter 的使用：Finagle
+     *
+     * Jeff Smick，Twitter 软件工程师
+     *
+     * Finagle 是 Twitter 构建在 Netty 之上的容错的、协议不可知的 RPC 框架。从提供用户信息、 推特以及时间线
+     * 的后端服务到处理 HTTP 请求的前端 API 端点，所有组成 Twitter 架构的核心服务都建立在 Finagle 之上。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
