c00bb62960895a5e132fa25d0c854fd672970b27
siwuxie095 <834879583@qq.com>
2020-12-05 14:28:45 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter14th/example22th/Main.java
@@ -0,0 +1,20 @@
+package com.siwuxie095.network.chapter14th.example22th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-05 14:23:10
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里旨在揭示真实世界中的 Netty 的使用场景，以及它是如何帮助这些公司解决了重大的网络通信问题的。
+     * 值得注意的是，在所有的场景下，Netty 都不仅是被作为一个代码框架而使用，而且还是开发和架构最佳实
+     * 践的重要组成部分。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
