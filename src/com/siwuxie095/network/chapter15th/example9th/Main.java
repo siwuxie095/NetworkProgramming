bab3730664e1b7183113c065d55b4789eea11910
siwuxie095 <834879583@qq.com>
2020-12-05 21:02:52 +0800

    Facebook 小结；

---
+++ src/com/siwuxie095/network/chapter15th/example9th/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.network.chapter15th.example9th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-05 21:01:05
+ */
+public class Main {
+
+    /**
+     * Facebook 小结
+     *
+     * 在 Netty 的帮助下，他们已经能够构建更好的 Java 服务器框架了，其几乎能够与他们最快的 C++ Thrift 服务器框架
+     * 的性能相媲美。他们已经将他们现有的一些主要的 Java 服务迁移到了 Nifty，并解决了一些令人讨厌的稳定性和性能问题，
+     * 同时他们还开始将一些来自 Netty，以及 Nifty 和 Swift 开发过程中的思想，反馈到提高 C++ Thrift 的各个方面中。
+     *
+     * 不仅如此，使用 Netty 是令人愉悦的，并且它已经添加了大量的新特性，例如，对于 Thrift 客户端的内置 SOCKS 支持
+     * 来说，添加起来小菜一碟。
+     *
+     * 但是他们并不止步于此。他们还有大量的性能调优工作要做，以及针对将来的大量的其他方面的改进计划。如果你对使用 
+     * Java 进行 Thrift 开发感兴趣，一定要关注哦！
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
