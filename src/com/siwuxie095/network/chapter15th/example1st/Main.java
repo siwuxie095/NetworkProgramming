a766491006d85874d28b499f5119194e6e5b4d10
siwuxie095 <834879583@qq.com>
2020-12-05 18:31:20 +0800

    案例研究，第二部分；

---
+++ src/com/siwuxie095/network/chapter15th/example1st/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter15th.example1st;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-05 14:30:02
+ */
+public class Main {
+
+    /**
+     * 案例研究，第二部分
+     *
+     * 在这里，将看到 Facebook 和 Twitter（两个最流行的社交网络）是如何使用 Netty 的。他们都利用了 Netty
+     * 灵活和通用的设计来构建框架和服务，以满足对极端伸缩性以及可扩展性的需求。
+     * 
+     * 这里所呈现的案例研究都是由那些负责设计和实现所述解决方案的工程师所撰写的。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
