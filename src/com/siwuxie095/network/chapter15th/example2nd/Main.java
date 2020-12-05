33acf149d959926ffac84311c1019035980a68e3
siwuxie095 <834879583@qq.com>
2020-12-05 18:36:34 +0800

    Netty 在 Facebook 的使用：Nifty 和 Swift；

---
+++ src/com/siwuxie095/network/chapter15th/example2nd/Main.java
@@ -0,0 +1,21 @@
+package com.siwuxie095.network.chapter15th.example2nd;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-05 18:34:28
+ */
+public class Main {
+
+    /**
+     * Netty 在 Facebook 的使用：Nifty 和 Swift
+     *
+     * Andrew Cox，Facebook 软件工程师
+     *
+     * 在 Facebook，他们在他们的几个后端服务中使用了 Netty（用于处理来自手机应用程序的消息通信、用于 HTTP 客户端等），
+     * 但是他们增长最快的用法还是通过他们所开发的用来构建 Java 的 Thrift 服务的两个新框架：Nifty 和 Swift。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
