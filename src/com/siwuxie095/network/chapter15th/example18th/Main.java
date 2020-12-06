82bdf7820b764acce8674122ac2df9bcf9e2449a
siwuxie095 <834879583@qq.com>
2020-12-06 11:22:24 +0800

    Twitter 小结；

---
+++ src/com/siwuxie095/network/chapter15th/example18th/Main.java
@@ -0,0 +1,25 @@
+package com.siwuxie095.network.chapter15th.example18th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-06 11:19:36
+ */
+public class Main {
+
+    /**
+     * Twitter 小结
+     *
+     * Finagle 已经取得了辉煌的成绩。他们已经想方设法大幅度地提高了他们所能够处理的流量，同时也降低了延迟
+     * 以及硬件需求。例如，在将他们的 API 端点从 Ruby 技术栈迁移到 Finagle 之后，可以看到，延迟从数百毫
+     * 秒下降到了数十毫秒之内，同时还将所需要的机器数量从三位数减少到了个位数。他们新的技术栈已经使得他们达
+     * 到了新的吞吐量记录。
+     *
+     * Finagle 的诞生是为了满足 Twitter 横向扩展以支持全球数以亿计的用户的需求，而在当时支撑数以百万计的
+     * 用户并保证服务在线已然是一项艰巨的任务了。使用 Netty 作为基础，他们能够快速地设计和建造 Finagle，
+     * 以解决他们的伸缩性难题。Finagle 和 Netty 处理了 Twitter 所遇到的每一个请求。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
