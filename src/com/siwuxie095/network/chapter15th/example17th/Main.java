f67ce2b6894077537e9f864d23891c2b5eed014c
siwuxie095 <834879583@qq.com>
2020-12-06 11:18:10 +0800

    未来：Netty；

---
+++ src/com/siwuxie095/network/chapter15th/example17th/Main.java
@@ -0,0 +1,19 @@
+package com.siwuxie095.network.chapter15th.example17th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-06 11:16:25
+ */
+public class Main {
+
+    /**
+     * 未来：Netty
+     *
+     * 为了改善 Netty 的各个部分，让 Finagle 以及更加广泛的社区都能够从中受益，他们一直在与 Netty 的维护者密切合作。
+     * 最近，Finagle 的内部结构已经升级为更加模块化的结构，为升级到 Netty 4 铺平了道路。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
