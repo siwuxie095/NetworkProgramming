207048e1974ee8b13bafaa823bd019129757b6a1
siwuxie095 <834879583@qq.com>
2020-12-06 11:26:22 +0800

    小结；

---
+++ src/com/siwuxie095/network/chapter15th/example19th/Main.java
@@ -0,0 +1,27 @@
+package com.siwuxie095.network.chapter15th.example19th;
+
+/**
+ * @author Jiajing Li
+ * @date 2020-12-06 11:23:15
+ */
+public class Main {
+
+    /**
+     * 小结
+     *
+     * 这里深入了解了对于像 Facebook 以及 Twitter 这样的大公司是如何使用 Netty 来保证最高水准的性能
+     * 以及灵活性的。
+     *
+     * （1）Facebook 的 Nifty 项目展示了如何通过提供自定义的协议编码器以及解码器，利用 Netty 来替换
+     * 现有的 Thrift 实现。
+     *
+     * （2）Twitter 的 Finagle 展示了如何基于 Netty 来构建你自己的高性能框架，并通过类似于负载均衡以
+     * 及故障转移这样的特性来增强它的。
+     *
+     * 希望这里所提供的案例研究，能够成为你打造下一代杰作的时候的信息和灵感的来源。
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
